2ac14377fa42a3f1489792857a5b34d0799e0b09
Jacques Vella Critien <vellacritien@gmail.com>
2024-04-19 11:26:56 +0200

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"XFp94PEsGdDO+c1cILvuRD4rFamnMN5k3wCVsl6JOn4=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"MDgrE6oJ6126rOvc9KUc1lMkM+uZzzCneM/RHVVNhI4=\"}"
